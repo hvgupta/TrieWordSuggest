67c0bbfd2b121b2d7d752bcab5f9a64acc176e7c
hvgupta <hvgupta@outlook.in>
2025-07-31 00:33:26 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,42 @@
-mod words_reader;
 mod trie;
+mod words_reader;
 
+use lazy_static::lazy_static;
+use regex::Regex;
+use trie::Trie;
 use words_reader::WordIterator;
 
+lazy_static! {
+    static ref WORD_REGEX: Regex = Regex::new(r"^[a-zA-Z]+$").unwrap();
+}
+
 fn main() {
-    let word_reader = WordIterator::new("./data/words.txt");
+    let mut trie = Trie::new();
+    let word_reader = match WordIterator::new("./data/words.txt") {
+        Ok(reader) => reader,
+        Err(e) => {
+            eprintln!("Error reading words file: {}", e);
+            return;
+        }
+    };
+    for result in word_reader {
+        match result {
+            Ok(word) => {
+                if WORD_REGEX.is_match(&word) {
+                    trie.insert(&word);
+                } else {
+                    eprintln!("Invalid word found: {}", word);
+                }
+            }
+            Err(_) => {
+                continue;
+            }
+        }
+    }
+
+    let found = trie.find("example");
+    println!("Word 'example' found: {}", found);
+
+    let closest_words = trie.suggest("profide", 3);
+    println!("Suggestions for 'exampl': {:?}", closest_words);
 }
--- src/trie/suggest.rs
+++ src/trie/suggest.rs
@@ -5,14 +5,14 @@ use crate::trie::node::TrieNode;
 use priority_queue::PriorityQueue;
 use std::cmp::Reverse;
 
-
-
 impl Trie {
-    pub fn suggest(&self, word: &str) -> Vec<String> {
+    pub fn suggest(&self, word: &str, max_dist: u8) -> Vec<String> {
         let mut top_queue = PriorityQueue::<String, Reverse<u8>>::new();
-        let mut dfs = Vec::<(&TrieNode, String, usize, u8)>::new();
+        let mut dfs = Vec::<(&TrieNode, String, usize, i8)>::new();
 
-        dfs.push((&self.root, String::new(), 0, word.len() as u8));
+        let len = word.len() as i8;
+
+        dfs.push((&self.root, String::new(), 0, len));
 
         while !dfs.is_empty() {
             let (node, current_word, index, dist) = dfs.pop().unwrap();
@@ -22,23 +22,18 @@ impl Trie {
             };
             let matched_child = node.children.get(&index_char);
             if let Some(child) = matched_child {
-                let next_word = current_word.clone() + &index_char.to_string();
-                dfs.push((
-                    child,
-                    next_word.clone(),
-                    index + 1,
-                    dist - 1,
-                ));
+                let next_word = current_word.clone() + &child.letter.to_string();
+                dfs.push((child, next_word.clone(), index + 1, dist - 1));
             }
 
-            if dist > self.max_word_diff + word.len() as u8 {
+            if dist > self.max_word_diff + word.len() as i8 || dist < -self.max_word_diff {
                 continue; // Skip if distance exceeds max allowed
             }
 
-            if dist < self.max_word_diff {
-                top_queue.push(current_word.clone(), Reverse(dist));
+            if (dist.abs() <= self.max_word_diff) && node.is_word {
+                top_queue.push(current_word.clone(), Reverse(dist.abs() as u8));
             }
-            
+
             let close_keys = get_keywords_at_dist(&index_char, &2);
             for key in close_keys {
                 let matched_child = match node.children.get(&key) {
@@ -47,23 +42,27 @@ impl Trie {
                 };
                 dfs.push((
                     matched_child,
-                    current_word.clone() + &key.to_string(),
+                    current_word.clone() + &matched_child.letter.to_string(),
                     index + 1,
                     dist + 1,
                 ));
             }
         }
 
-        let mut suggested_words = Vec::new(); // Placeholder for the actual suggestions
+        let mut suggested_words = Vec::new();
         let (word, smallest_dist) = match top_queue.pop() {
             Some((word, Reverse(dist))) => (word, dist),
-            None => return suggested_words, 
+            None => return suggested_words,
         };
+        
+        if smallest_dist > max_dist {
+            return suggested_words; // If the smallest distance is greater than max_dist, return empty
+        }
 
         suggested_words.push(word);
 
-        while let Some((word, Reverse(_smallest_dist))) = top_queue.pop() {
-            if _smallest_dist != smallest_dist {
+        while let Some((word, Reverse(dist))) = top_queue.pop() {
+            if  dist > max_dist{
                 break; // Stop if we reach a word with a different distance
             }
             suggested_words.push(word);
